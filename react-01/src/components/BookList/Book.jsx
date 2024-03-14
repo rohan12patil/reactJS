5cf1eecc62fae587b2b31f34a88ceb599371dae9
experfy-rohanpatil <rpatil@experfy.com>
2024-03-14 22:41:31 +0000

    Props Destructing #2

--- react-01/src/components/BookList/Book.jsx
+++ react-01/src/components/BookList/Book.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
-const Book = (props) => {
-  const {img, title, author} = props;
+const Book = ({img, title, author}) => {
   return (
     <div className='book'>
       <h2>{title}</h2>
