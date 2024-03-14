2fccc83aa2108f2177cb6baa3d6a32620e17df87
experfy-rohanpatil <rpatil@experfy.com>
2024-03-14 22:04:47 +0000

    Refactored Book Component

--- react-01/src/components/BookList/Book.jsx
+++ react-01/src/components/BookList/Book.jsx
@@ -1,28 +1,15 @@
-
-const Book = ()=>{
-return <div className="book">
-    <Title/>
-    <BookImg/>
-    <Author/>
-    <br/>
-</div>
-}
-
-export default Book;
-
-const Title = () => {
-  return <h2>Boy with Flowers Book</h2>;
-};
-
-const BookImg = () =>{
-    return (
+const Book = () => {
+  return (
+    <div className='book'>
+      <h2>Boy with Flowers Book</h2>
       <img
         src='https://m.media-amazon.com/images/I/51EFk4pM8PL.AC_SX250.jpg'
         alt='BookImage'
       ></img>
-    );
-}
+      <h3>Author: Jarvis</h3>
+      <br />
+    </div>
+  );
+};
 
-const Author = ()=>{
-    return <h3>Author: Jarvis</h3>;
-}
+export default Book;
