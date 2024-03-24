b7e602b936b9805996cf53c20b19ab034483d840
experfy-rohanpatil <rpatil@experfy.com>
2024-03-24 17:42:20 +0000

    context api : default cleared

--- react-01/src/components/BookList/Book.jsx
+++ react-01/src/components/BookList/Book.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
 const Book = (props) => {
-  const {img, title, author, children} = props;
+  const { img, title, author, children } = props;
   return (
     <div className='book'>
       <h2>{title}</h2>
-      <img src={img}/>
+      <img src={img} />
       <h3>{author}</h3>
       {children}
     </div>
