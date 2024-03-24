9fae61f384221007e88ff761fc05a2685dbab0b0
experfy-rohanpatil <rpatil@experfy.com>
2024-03-24 17:52:49 +0000

    Context Api: Navbar, NavLinks, UserContainer  components added

---
+++ 09-context-api/src/prop-drilling/NavLinks.jsx
@@ -0,0 +1,18 @@
+import UserContainer from "./UserContainer";
+
+const NavLinks = () => {
+  return (
+    <>
+      <ul>
+        <li>
+          <a href='#'>Home</a>
+        </li>
+        <li>
+          <a href='#'>About</a>
+        </li>
+      </ul>
+      <UserContainer/>
+    </>
+  );
+};
+export default NavLinks;
